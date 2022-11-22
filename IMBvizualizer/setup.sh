9819e70a9af4012adf4c0131f297a291ffdddef2
mthane <mthane93@gmail.com>
2022-11-23 00:34:00 +0100

    Update setup.sh

--- IMBvizualizer/setup.sh
+++ IMBvizualizer/setup.sh
@@ -1,5 +1,4 @@
-sudo apt install build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev libfontconfig1-dev
-sudo apt-get install r-cran-devtools
+sudo apt install build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev libfontconfig1-dev libharfbuzz-dev libfribidi-dev
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo apt update && sudo apt upgrade
