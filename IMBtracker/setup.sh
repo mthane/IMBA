17d2823e0e897e640c9e42b13d3e16836af850c3
mthane <44970717+mthane@users.noreply.github.com>
2022-11-22 11:44:33 +0100

    Update setup.sh

--- IMBtracker/setup.sh
+++ IMBtracker/setup.sh
@@ -3,8 +3,9 @@ sudo apt install python3
 sudo apt --only-upgrade install python3
 sudo apt install python3-pip
 sudo apt install  -force-yes libxkbcommon-x11-0 libx11-xcb1 liblpsolve55-dev libalglib-dev libboost-all-dev libopencv-dev build-essential cmake git unzip
-bash IMBtracker_cpp/setup.sh
-
+cd IMBtracker_cpp
+setup.sh
+cd ..
 sudo apt install python-setuptools-scm
 pip3 install --upgrade setuptools pip
 pip3 install scikit-build
