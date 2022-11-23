8c9115dfaf71c8b7d1e307480c32273049b11e0b
mthane <mthane93@gmail.com>
2022-11-23 01:07:02 +0100

    Update setup.sh

--- IMBvizualizer/setup.sh
+++ IMBvizualizer/setup.sh
@@ -1,4 +1,6 @@
-sudo apt install build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev libfontconfig1-dev libharfbuzz-dev libfribidi-dev
+sudo apt install build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev libfontconfig1-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev libcairo2-dev
+sudo apt install cmake
+
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo apt update && sudo apt upgrade
