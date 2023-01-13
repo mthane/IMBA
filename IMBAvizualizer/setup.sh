682d30849977ed5fe48c14f7dd2f594c81c4070e
mthane <mthane93@gmail.com>
2023-01-13 11:54:19 +0100

    change cran

--- IMBAvizualizer/setup.sh
+++ IMBAvizualizer/setup.sh
@@ -1,7 +1,7 @@
 sudo apt install build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev libfontconfig1-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev libcairo2-dev
 sudo apt install cmake
 
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran40/'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo apt update && sudo apt upgrade
 sudo apt install r-base
