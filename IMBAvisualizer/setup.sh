3adc67b98e7bb9620718bf8442d40151a4464d6e
mthane <44970717+mthane@users.noreply.github.com>
2023-01-27 14:42:21 +0100

    Update setup.sh

--- IMBAvisualizer/setup.sh
+++ IMBAvisualizer/setup.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo apt update && sudo apt upgrade
 sudo apt install r-base
-
+cd R
 sudo -i R
 install.packages("devtools")
 library(devtools)
